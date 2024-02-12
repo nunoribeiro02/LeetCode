89ea32101a0dd83f822a88f8062351d901dae3fb
nunoribeiro02 <nuno.m.p.ribeiro@tecnico.ulisboa.pt>
2024-02-12 22:13:17 +0000

    feat: more effient algorithm for 169 majority element in java

--- Java/169-MajorityElement.java
+++ Java/169-MajorityElement.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+/* 
 class Solution {
     public int majorityElement(int[] nums) {
         // count<n, times it appears>
@@ -21,3 +22,31 @@ class Solution {
         return majority;
     }
 }
+
+Space: O(k), k is the number of distinct elements of the array
+Time: O(n), n is the size of the array
+*/
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int majority = 0;
+        int count = 0;
+
+        for (int n : nums){
+            // Reset majority candidate
+            if (count == 0) {
+                majority = n;
+                count = 1;
+            }
+            else if (majority == n) count += 1;
+            else if (majority != n) count -= 1;
+        }
+
+        return majority;
+    }
+}
+
+/*
+ Space: O(1)
+ Time: O(n), n is the size of the array
+ */
